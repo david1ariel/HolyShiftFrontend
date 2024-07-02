42d24be0607cd22062b96d8b80aabcd7bb0637d7
David Ariel <david1ariel@gmail.com>
2024-07-02 21:00:58 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-layout class="app">
+    <div class="main">
+      <div class="menu"></div>
+      <div class="content">
+
+        <q-page-container>
+      <router-view />
+    </q-page-container>
+
+      </div>
+    </div>
+
+    <div class="footer"></div>
+
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Pages </q-item-label>
+
+        <div v-for="page in pages" :key="page">{{ page }}</div>
+      </q-list>
+    </q-drawer> -->
+
+    <!-- <q-page-container>
+      <router-view />
+    </q-page-container> -->
+  </q-layout>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+}
+.main {
+  display: flex;
+  height: 97%;
+  width: 100%;
+}
+.menu {
+  background-color: #19dbd0;
+  height: 100%;
+  width: 20%;
+  border-radius: 0 0 50px 0;
+}
+.content {
+  flex-grow: 1;
+}
+.footer {
+  flex-grow: 1;
+  width: 100%;
+}
+</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-page class="login-wrapper">
+    <div class="login-content">
+      <div class="title">Credentials, please.</div>
+      <div class="inputs">
+        <div class="input-wrapper">Username: <input class="text-input" /></div>
+        <div class="input-wrapper">Password: <input class="text-input" /></div>
+      </div>
+      <button class="login-btn" @click="loginClicked">Login</button>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+const loginClicked = () => {
+  alert('login clicked')
+}
+</script>
+
+
+
+<style scoped>
+.login-wrapper {
+  display: flex;
+  /* height: 100%; */
+  justify-content: space-around;
+  align-items: center;
+}
+.login-content {
+  min-width: 600px;
+}
+.title {
+  font-size: xxx-large;
+  text-align: center;
+}
+.text-input {
+  border: 0;
+  color: #128c8a;
+}
+input:focus {
+  outline: none;
+}
+.inputs {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+}
+.input-wrapper {
+  width: 45%;
+  padding-left: 16px;
+  border-width: 0 0 3px 3px;
+  border-style: solid;
+  border-color: #19dbd0;
+}
+
+.login-btn {
+  width: 100%;
+  background-color: #19dbd0;
+  border-radius: 10px;
+  border: 0;
+  margin-top: 16px;
+  color: white;
+  font-size: x-large;
+  padding: 10px
+}
+</style>
